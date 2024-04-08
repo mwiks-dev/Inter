20b3a9e2e523a4255dd945d9ee75759d4e795499
Mwikali <mwikali119@gmail.com>
2024-04-08 15:38:14 +0300

    Mask date correctly

--- int.py
+++ int.py
@@ -1,16 +1,16 @@
 def mask(first_name, last_name, date_of_birth, email):
-    def mask_string(string):
-        masked = string[:2]
-        for char in string[2:]:
-            if char == '-':
-                masked += '-'
-            else:
-                masked += '*'
-
-        return masked
+    def mask_string(string, is_date= False):
+        if not is_date:
+            return string[:2] + '*' * (len(string) - 2)
+        else:
+            year, month, day = string.split('-')
+            masked_year = year[:2] + '**'
+            masked_month = month[0] + '*'
+            masked_day = day[0] + '*'
+            return '-'.join([masked_year, masked_month, masked_day])  
     masked_first_name = mask_string(first_name)
     masked_last_name = mask_string(last_name)
-    masked_dob = mask_string(date_of_birth)
+    masked_dob = mask_string(date_of_birth, is_date=True)
     masked_email = mask_string(email)
 
     return {"first_name": masked_first_name,
@@ -20,4 +20,4 @@ def mask(first_name, last_name, date_of_birth, email):
     
     
 
-print(mask('Maryann','Mwikali','1997-02-21','mwikali119@gmail.com'))
+print(mask('Maryann','Mwikali',"1997-02-21",'mwikali119@gmail.com'))
